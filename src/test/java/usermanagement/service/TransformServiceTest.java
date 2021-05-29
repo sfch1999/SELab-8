f893bd53d61e13622379a1316dbb5d9417737973
ashkanlarni <ash.larni@gmail.com>
2021-05-29 16:27:55 +0430

    TransformServiceTest modified.

--- src/test/java/usermanagement/service/TransformServiceTest.java
+++ src/test/java/usermanagement/service/TransformServiceTest.java
@@ -1,3 +1,47 @@
 package usermanagement.service;
 
-public class TransformServiceTest { }
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import usermanagement.entity.Person;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TransformServiceTest {
+
+    @InjectMocks
+    private TransformService transformService;
+
+    @Test
+    public void toUserDomainTest() {
+        Person person = new Person();
+
+        person.setfName("fName");
+        person.setlName("lName");
+        person.setCompanyName("company");
+
+        User user = transformService.toUserDomain(person);
+
+        assertEquals("fName", user.getFirstName());
+        assertEquals("lName", user.getLastName());
+        assertEquals("company", user.getCompanyName());
+    }
+
+    @Test
+    public void toUserEntityTest() {
+        User user = new User();
+
+        user.setFirstName("fName");
+        user.setLastName("lName");
+        user.setCompanyName("company");
+
+        Person person = transformService.toUserEntity(user);
+
+        assertEquals("fName", person.getfName());
+        assertEquals("lName", person.getlName());
+        assertEquals("company", person.getCompanyName());
+    }
+}
